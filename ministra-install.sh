77b613e08a72c99397db72955edc81a1f58d496e
2mesistemas <90299970+2mesistemas@users.noreply.github.com>
2021-09-21 01:15:33 -0300

    Update ministra-install.sh

--- ministra-install.sh
+++ ministra-install.sh
@@ -115,7 +115,30 @@ unzip ministra-5.6.5.zip -d /var/www/
 cd /var/www/stalker_portal/deploy
 phing
 cd ~
+STALKER="/var/www/stalker_portal"
+
+if [ ! -d $STALKER/deploy ]; then
+        wget -O /tmp/stalker.zip https://portal.ottg.de/fl/Ministra_TV_Platform_5.6.6.zip
+        unzip /tmp/stalker.zip -d /var/www/
+        mv /var/www/stalker_portal-*/* /var/www/stalker_portal/
+        rm -rf /var/www/stalker_portal-*
+fi
+
+if [ ! -s $STALKER/server/custom.ini ]; then
+        #wget -O $STALKER/server/custom.ini https://raw.githubusercontent.com/sybdata/Ministra/master/docker/stalker_custom.ini
+         wget -O $STALKER/server/custom.ini https://raw.githubusercontent.com/2mesistemas/iptv/main/stalker_custom.ini
+fi
+
+sed -i '/\bmysql_tzinfo_to_sql\b/g' $STALKER/deploy/build.xml
+
+cd $STALKER/deploy && phing
+if [ $? -eq 1 ]; then
+        phing
+fi
+
 
 # As variáveis de ambiente serão limpas. Obs: (Isto vai ocorrer automaticamente após um logout)
 printf "Limpando o ambiente\n"
 unset DEBIAN_FRONTEND
+
+echo Instalação terminada com Successo!
